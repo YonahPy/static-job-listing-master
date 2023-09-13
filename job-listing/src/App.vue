e5479d8b758f4b89dc2fce099c9ee16cdf91ab61
YonahPy <jrjonas487@gmail.com>
2023-09-13 10:25:43 -0300

    .

--- job-listing/src/App.vue
+++ job-listing/src/App.vue
@@ -1,11 +1,37 @@
 <template>
-  
+  <header></header>
+  <main>
+    <jobs v-for="job in data" :key="job.id" :dataJobs="job">
+        
+    </jobs>
+  </main>
 </template>
 
 <script>
-
+import jobs from './components/jobs.vue'
+import data from './assets/data.json'
+export default{
+    components:{
+        jobs
+    },
+    data(){
+        return{
+            data: data
+        }
+    }
+}
 </script>
 
-<style scoped>
- 
+<style>
+ *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+ }
+ header{
+    background-image: url('./assets/images/bg-header-desktop.svg');
+    height: 20vh;
+    background-color: hsl(180, 29%, 50%);
+    
+ }
 </style>
---
+++ job-listing/src/components/jobs.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <img :src="dataJobs.logo" :alt="dataJobs.company">
+    <div>
+        <div>
+            {{ dataJobs.company }} {{ dataJobs.new }} {{ dataJobs.featured }}
+        </div>
+        <div>
+            {{ dataJobs.position }}
+        </div>
+        <div>
+            {{dataJobs.postedAt }} {{ dataJobs.contract }} {{ dataJobs.location }}
+        </div>
+
+    </div>
+
+    <div>
+        {{ dataJobs.role }}  {{ dataJobs.level }}
+        <div v-for="job in dataJobs.languages" :key="dataJobs.id">
+            {{ job }}
+        </div>
+
+    </div>
+</div>
+</template>
+
+<script>
+    export default{
+        props: ['dataJobs']
+    }
+
+</script>
+
+<style>
+
+</style>
